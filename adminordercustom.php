5ab6a9b36426b8ada27c7c75430a53be47e1fcb7
Samuel Bouye <bouyesamuel@gmail.com>
2021-08-26 10:42:07 +0000

    display custom information on admin order

---
+++ adminordercustom.php
@@ -0,0 +1,87 @@
+<?php
+/*
+* 2007-2016 PrestaShop
+*
+* NOTICE OF LICENSE
+*
+* This source file is subject to the Academic Free License (AFL 3.0)
+* that is bundled with this package in the file LICENSE.txt.
+* It is also available through the world-wide-web at this URL:
+* http://opensource.org/licenses/afl-3.0.php
+* If you did not receive a copy of the license and are unable to
+* obtain it through the world-wide-web, please send an email
+* to license@prestashop.com so we can send you a copy immediately.
+*
+* DISCLAIMER
+*
+* Do not edit or add to this file if you wish to upgrade PrestaShop to newer
+* versions in the future. If you wish to customize PrestaShop for your
+* needs please refer to http://www.prestashop.com for more information.
+*
+*  @author PrestaShop SA <contact@prestashop.com>
+*  @copyright  2007-2016 PrestaShop SA
+*  @license    http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
+*  International Registered Trademark & Property of PrestaShop SA
+*/
+
+class Adminordercustom extends Module
+{
+    public function __construct()
+    {
+        $this->name = 'adminordercustom';
+        $this->version = '1.0.0';
+        $this->author = 'Eliphaz';
+        $this->need_instance = 0;
+
+        $this->bootstrap = true;
+
+        parent::__construct();
+
+        $this->displayName = $this->getTranslator()->trans(
+            'Admin order custom',
+            [],
+            'Modules.Adminordercustom.Admin'
+        );
+
+        $this->description =
+            $this->getTranslator()->trans(
+                'Add custom text on Admin order',
+                [],
+                'Modules.Adminordercustom.Admin'
+            );
+
+        $this->ps_versions_compliancy = [
+            'min' => '1.7.7.0',
+            'max' => _PS_VERSION_,
+        ];
+    }
+
+
+    /**
+     * This function is required in order to make module compatible with new translation system.
+     *
+     * @return bool
+     */
+    public function isUsingNewTranslationSystem()
+    {
+        return true;
+    }
+
+
+    public function install()
+    {
+        return parent::install()
+            && $this->registerHook('displayAdminOrder');
+    }
+
+
+    public function uninstall()
+    {
+        return parent::uninstall();
+    }
+
+    public function hookDisplayAdminOrder($params)
+    {
+        return $this->display(__FILE__, 'views/templates/admin/order.tpl');
+    }
+}
